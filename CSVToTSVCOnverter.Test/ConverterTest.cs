cf8087fe5f13989ec0880ff5eb9a876b693f0166
Arjun Mukundan <arjunmukundan@outlook.in>
2024-09-23 17:17:27 +0530

    Added Test Project to the solution

---
+++ CSVToTSVCOnverter.Test/ConverterTest.cs
@@ -0,0 +1,104 @@
+﻿using System.Reflection;
+
+namespace CSVToTSVConverter.Test
+{
+    public class ConverterTest
+    {
+        /// <summary>
+        /// Checks whether the first 2 lines are matching in X direction
+        /// </summary>
+        [Fact]
+        public void ConvertToTSVFile_ShouldMatchFirstTwoLinesInXDirection()
+        {
+            // Arrange
+            string inputFilePath = Path.GetDirectoryName(Assembly.GetExecutingAssembly().Location) + "\\Input\\currency.csv";
+            string outputFilePath = Path.GetDirectoryName(Assembly.GetExecutingAssembly().Location) + "\\Output\\currency.tsv";
+
+            // Act
+            Converter.ConvertToTSVFile(inputFilePath, outputFilePath, 'x', 2);
+
+            // Assert
+            var tsvOutput = File.ReadAllLines(outputFilePath);
+            // Since we have header too we have 3 rows
+            Assert.Equal(3, tsvOutput.Length);
+            Assert.Equal("AED\tد.إ\tUnited Arab Emirates d", tsvOutput[1]);
+            Assert.Equal("AFN\t؋\tAfghan afghani", tsvOutput[2]);
+        }
+
+        /// <summary>
+        /// Checks whether the first 2 lines are matching in Y direction
+        /// </summary>
+        [Fact]
+        public void ConvertToTSVFile_ShouldMatchFirstTwoLinesInYDirection()
+        {
+            // Arrange
+            string inputFilePath = Path.GetDirectoryName(Assembly.GetExecutingAssembly().Location) + "\\Input\\currency.csv";
+            string outputFilePath = Path.GetDirectoryName(Assembly.GetExecutingAssembly().Location) + "\\Output\\currency.tsv";
+
+            // Act
+            Converter.ConvertToTSVFile(inputFilePath, outputFilePath, 'y', 2);
+
+            // Assert
+            var tsvOutput = File.ReadAllLines(outputFilePath);
+            // Since we have header too we have 3 rows
+            Assert.Equal(3, tsvOutput.Length);
+            Assert.Equal("ZAR\tR\tSouth African rand", tsvOutput[1]);
+            Assert.Equal("ZMW\tZK\tZambian kwacha", tsvOutput[2]);
+        }
+
+        /// <summary>
+        /// Convert all rows if no line number provided
+        /// </summary>
+        [Fact]
+        public void ConvertToTSVFile_ShouldConvertAllRowsForNoLineNumber()
+        {
+            // Arrange
+            string inputFilePath = Path.GetDirectoryName(Assembly.GetExecutingAssembly().Location) + "\\Input\\currency.csv";
+            string outputFilePath = Path.GetDirectoryName(Assembly.GetExecutingAssembly().Location) + "\\Output\\currency.tsv";
+
+            // Act
+            Converter.ConvertToTSVFile(inputFilePath, outputFilePath, 'x');
+
+            // Assert
+            var tsvOutput = File.ReadAllLines(outputFilePath);
+            // Since we have test file has 164 rows
+            Assert.Equal(164, tsvOutput.Length);
+        }
+
+        /// <summary>
+        /// Convert all rows if no line number and direction is  provided 
+        /// </summary>
+        [Fact]
+        public void ConvertToTSVFile_ShouldConvertAllRowsForNoDirectionAndLineNumber()
+        {
+            // Arrange
+            string inputFilePath = Path.GetDirectoryName(Assembly.GetExecutingAssembly().Location) + "\\Input\\currency.csv";
+            string outputFilePath = Path.GetDirectoryName(Assembly.GetExecutingAssembly().Location) + "\\Output\\currency.tsv";
+
+            // Act
+            Converter.ConvertToTSVFile(inputFilePath, outputFilePath);
+
+            // Assert
+            var tsvOutput = File.ReadAllLines(outputFilePath);
+            // Since we have test file has 164 rows
+            Assert.Equal(164, tsvOutput.Length);
+        }
+
+        /// <summary>
+        /// Throw file not found if file not found in location
+        /// </summary>
+        [Fact]
+        public void ConvertToTSVFile_ShouldThrowFileNotFoundException()
+        {
+            // Arrange
+            string inputFilePath = Path.GetDirectoryName(Assembly.GetExecutingAssembly().Location) + "\\Input\\currency_error.csv";
+            string outputFilePath = Path.GetDirectoryName(Assembly.GetExecutingAssembly().Location) + "\\Output\\currency.tsv";
+
+            // Act + Assert
+            var exception = Assert.Throws<FileNotFoundException> (() => Converter.ConvertToTSVFile(inputFilePath, outputFilePath));
+
+            //Compare
+            Assert.Equal("File doesnot exists!!", exception.Message);
+        }
+    }
+}
--- CSVToTSVConverter/Program.cs
+++ CSVToTSVConverter/Program.cs
@@ -5,109 +5,39 @@ namespace CSVToTSVConverter
     {
         static void Main(string[] args)
         {
+
             string inputFilePath = "";
             string outputFilePath = "";
             char direction = 'x';
-            int noOfLines = 0;
-            try
+            int noOfLines = -1;
+
+            if (args.Any())
             {
-                if (args.Any())
+                inputFilePath = args[0];
+                outputFilePath = args[1];
+
+                try
                 {
-                    inputFilePath = args[0];
-                    outputFilePath = args[1];
-                    string headerRecord;
-                    Console.WriteLine("Loading csv file " + inputFilePath);
+                    // Since direction and nooflines are optional arguments, if it is not provided, we will just go forward with all records
+                    if (args.Count() > 2)
+                        char.TryParse(args[2], out direction);
+                    if (args.Count() > 3)
+                        int.TryParse(args[3], out noOfLines);
 
-                    if (File.Exists(inputFilePath))
-                    {
-                        // Reads all the lines of the file and converts to string collection
-                        IEnumerable<string>? listOfRecords = File.ReadAllLines(inputFilePath)?.ToList();
+                    Converter.ConvertToTSVFile(inputFilePath, outputFilePath, direction, noOfLines);
 
-                        if (listOfRecords?.Any() ?? false)
-                        {
-                            // Taking into consideration that we have header in the file, we don't want that row to be considered for selection
-                            // Removing Header Row and adding it to headerRow
-                            headerRecord = listOfRecords.First();
-                            IEnumerable<string> listOfRecordsToConvert = listOfRecords.Skip(1);
-                            // Since direction is optional argument, if it is not provided, we will just go forward with all records
-                            if (args.Count() > 2 && char.TryParse(args[2], out direction))
-                            {
-                                var directionText = (direction == 'x') ? "First" : "Last";
-                                if (args.Count() > 3 && int.TryParse(args[3], out noOfLines))
-                                {
-                                    if (noOfLines <= listOfRecordsToConvert.Count())
-                                    {
-                                        Console.WriteLine($"Fetching {directionText} {noOfLines} records in the file!!");
-                                        listOfRecordsToConvert = directionText == "First"
-                                                                                        ? listOfRecordsToConvert.Take(noOfLines)
-                                                                                        : listOfRecordsToConvert.TakeLast(noOfLines);
-                                    }
-                                    else
-                                    {
-                                        throw new Exception($"There are only {listOfRecordsToConvert.Count()} records in the file and you are looking for {noOfLines} records!!");
-                                    }
-
-                                }
-                            }
-
-                            // The converted header row is added back to the convertedRecords list
-                            var convertedRecords = new List<string>() { ReturnTabbedRecord(headerRecord) };
-                            convertedRecords.AddRange(ConvertToTSV(listOfRecordsToConvert));
-
-                            Console.WriteLine("Writing tsv file to " + outputFilePath);
-                            File.WriteAllLines(outputFilePath, convertedRecords);
-
-                            Console.WriteLine("Successfully Converted!");
-                        }
-                        else
-                        {
-                            Console.WriteLine("File is either empty or not valid");
-                        }
-                    }
-                    else
-                    {
-                        Console.WriteLine("File doesnot exists!!");
-                    }
                 }
-                else
+                catch (Exception ex)
                 {
-                    Console.WriteLine("No Arguments available");
+                    Console.WriteLine("The Conversion failed : " + ex.Message);
                 }
             }
-            catch (Exception ex)
+            else
             {
-                Console.WriteLine("The Conversion failed : " + ex.Message);
+                Console.WriteLine("No Arguments available");
             }
         }
-
-        /// <summary>
-        /// Conversion to the TSV
-        /// </summary>
-        /// <param name="listOfRecordsToConvert"></param>
-        /// <returns></returns>
-        private static IEnumerable<string> ConvertToTSV(IEnumerable<string> listOfRecordsToConvert)
-        {
-            var listOfRecords = new List<string>();
-
-            foreach (string record in listOfRecordsToConvert)
-            {
-                listOfRecords.Add((string)ReturnTabbedRecord(record));
-            }
-            return listOfRecords;
-        }
-
-        /// <summary>
-        /// Returns a tabbed record string
-        /// </summary>
-        /// <param name="record"></param>
-        /// <returns></returns>
-        private static string ReturnTabbedRecord(string record)
-        {
-            return record.Replace(',', '\t');
-        }
-
     }
-
 }
 
 
